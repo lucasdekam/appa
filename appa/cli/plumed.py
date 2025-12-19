fd2b5a335eccb4c902825f0c11b58a8253834821
Lucas <lucasdekam@gmail.com>
2025-12-19 20:22:53 +0100

    make plumed a group

--- appa/cli/plumed.py
+++ appa/cli/plumed.py
@@ -3,8 +3,13 @@ import click
 from appa.plumed import generate_plumed_volmer
 
 
-@click.command("plumed")
-@click.argument("setup")
+@click.group()
+def plumed():
+    """PLUMED input writing."""
+    pass
+
+
+@plumed.command("volmer")
 @click.option("--oxygen_id", "-o", type=int)
 @click.option("--hydrogen_id", "-h", type=int)
 @click.option("--surface_id", "-s", type=int)
@@ -14,8 +19,7 @@ from appa.plumed import generate_plumed_volmer
     type=float,
     help="Target value of the reaction coordinate ξ = d(O-H) - d(Pt-H) in Å.",
 )
-def plumed(
-    setup,
+def volmer(
     oxygen_id,
     hydrogen_id,
     surface_id,
@@ -24,14 +28,13 @@ def plumed(
     stride,
 ):
     """Write plumed.dat input file for a Volmer step calculation."""
-    if setup == "volmer":
-        generate_plumed_volmer(
-            oxygen_id,
-            hydrogen_id,
-            surface_id,
-            cv_target,
-            kappa,
-            stride,
-            colvar_file=f"COLVAR_{cv_target}",
-            outfile="plumed.dat",
-        )
+    generate_plumed_volmer(
+        oxygen_id,
+        hydrogen_id,
+        surface_id,
+        cv_target,
+        kappa,
+        stride,
+        colvar_file=f"COLVAR_{cv_target}",
+        outfile="plumed.dat",
+    )
