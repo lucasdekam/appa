7f1180f3ae921c2a9e2d5b59ce5986d8218a6f70
Lucas <lucasdekam@gmail.com>
2025-12-21 21:34:39 +0100

    add coverage options

--- appa/cli/build.py
+++ appa/cli/build.py
@@ -9,11 +9,13 @@ from appa.build import Electrode, Interface
     "-o",
     "--output",
     default="interface.xyz",
+    show_default=True,
     help="Output structure file",
 )
 @click.option(
     "--material",
     default="Au",
+    show_default=True,
     help="Chemical element of the electrode material",
 )
 @click.option(
@@ -28,41 +30,74 @@ from appa.build import Electrode, Interface
     "--d-water",
     type=float,
     default=10,
+    show_default=True,
     help="Water layer thickness",
 )
 @click.option(
     "--d-vacuum",
     type=float,
     default=20,
+    show_default=True,
     help="Vacuum layer thickness",
 )
 @click.option(
     "--a",
     type=float,
     default=None,
+    show_default=True,
     help="Lattice parameter; if None takes ASE default",
 )
 @click.option(
     "--fix-layers",
     type=int,
     default=2,
+    show_default=True,
     help="Number of layers to fix at bottom of slab",
 )
 @click.option(
     "--ion",
     type=str,
     default=None,
+    show_default=True,
     help="Chemical element of the ion",
 )
 @click.option(
     "--n-ions",
     type=int,
     default=0,
+    show_default=True,
     help="Number of ions to add",
 )
-def build(output, material, size, d_water, d_vacuum, a, fix_layers, ion, n_ions):
+@click.option(
+    "--coverage",
+    type=float,
+    default=0,
+    show_default=True,
+    help="Hydrogen coverage",
+)
+@click.option(
+    "--ptop",
+    type=float,
+    default=0.5,
+    show_default=True,
+    help="Probability for top site occupation; P(fcc) is 1-P(top)",
+)
+def build(
+    output,
+    material,
+    size,
+    d_water,
+    d_vacuum,
+    a,
+    fix_layers,
+    ion,
+    n_ions,
+    coverage,
+    ptop,
+):
     """Build an electrode-electrolyte interface."""
     electrode = Electrode(material, size, a, fix_layers)
+    electrode.add_hydrogens(coverage, ptop)
     ion_dict = None
     if ion is not None and n_ions > 0:
         ion_dict = {ion: 4 for _ in range(n_ions)}
