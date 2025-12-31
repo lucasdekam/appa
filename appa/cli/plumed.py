50acea052a1a1f7b81f5350a3a8650d75b97523b
Lucas <lucasdekam@gmail.com>
2025-12-31 21:45:50 +0100

    add second volmer test

--- appa/cli/plumed.py
+++ appa/cli/plumed.py
@@ -1,6 +1,7 @@
 import click
+from ase.io import read
 
-from appa.plumed import DEFAULT_STRIDE, generate_plumed_volmer
+from appa.plumed import DEFAULT_STRIDE, generate_plumed_volmer, generate_plumed_volmer_2
 
 
 @click.group()
@@ -31,6 +32,31 @@ def volmer(
         oxygen_id=oxygen_id,
         hydrogen_id=hydrogen_id,
         surface_id=surface_id,
+        stride=stride,
+        colvar_file=f"COLVAR",
+        outfile="plumed.dat",
+    )
+
+
+@plumed.command("volmer2")
+@click.argument("xyz")
+@click.option("--hydrogen_id", "-h", type=int)
+@click.option(
+    "--stride",
+    type=int,
+    default=DEFAULT_STRIDE,
+    help="How often to print to COLVAR",
+    show_default=True,
+)
+def volmer2(
+    xyz,
+    hydrogen_id,
+    stride,
+):
+    """Write plumed.dat input file for a Volmer step calculation."""
+    generate_plumed_volmer_2(
+        atoms=read(xyz),
+        hydrogen_id=hydrogen_id,
         stride=stride,
         colvar_file=f"COLVAR",
         outfile="plumed.dat",
--- appa/plumed.py
+++ appa/plumed.py
@@ -11,6 +11,52 @@ DEFAULT_STRIDE = 10
 # kappa: float = DEFAULT_KAPPA,
 
 
+def generate_plumed_volmer_2(
+    atoms: Atoms,
+    hydrogen_id: int,
+    stride: int = DEFAULT_STRIDE,
+    colvar_file: str = "COLVAR",
+    outfile: Optional[str] = None,
+):
+    z_pt = atoms.positions[atoms.symbols == "Pt", 2].max()
+    oxygen_indices = [atom.index + 1 for atom in atoms if atom.symbol == "O"]
+    oxygen_numbers = ",".join(map(str, oxygen_indices))
+    hydrogen_number = hydrogen_id + 1
+    plumed_input = f"""UNITS LENGTH=A ENERGY=eV TIME=ps
+
+oxygens: GROUP ATOMS={oxygen_numbers}
+proton: GROUP ATOMS={hydrogen_number}
+
+# CV1: Coordination Number (1.0 = bonded, 0.0 = desorbed)
+cn: COORDINATION GROUPA=proton GROUPB=oxygens R_0=1.1 NN=6 MM=12
+
+# CV2: Height above Pt surface
+pos_H: POSITION ATOM={hydrogen_number}
+z_H: COMBINE ARG=pos_H.z COEFFICIENTS=1.0 PARAMETERS={z_pt:.2f} POWERS=1 PERIODIC=NO
+
+# Walls
+uwall: UPPER_WALLS ARG=z_H AT=5.0 KAPPA=100.0 
+lwall: LOWER_WALLS ARG=z_H AT=-1.0 KAPPA=100.0
+
+# Metadynamics
+metad: METAD ...
+    ARG=cn,z_H
+    PACE=500 HEIGHT=0.005 SIGMA=0.1,0.1
+    GRID_MIN=-0.1,-1.0 GRID_MAX=3.0,6.0 GRID_BIN=300,300
+    BIASFACTOR=10.0 TEMP=300.0 FILE=HILLS
+...
+
+# Output
+PRINT STRIDE={stride} ARG=metad.bias,uwall.bias,d_OH,d_MH FILE={colvar_file}
+"""
+
+    if outfile is not None:
+        with open(outfile, "w") as f:
+            f.write(plumed_input)
+
+    return plumed_input
+
+
 def generate_plumed_volmer(
     oxygen_id: int,
     hydrogen_id: int,
